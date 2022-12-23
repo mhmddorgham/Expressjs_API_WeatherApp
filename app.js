8117f9e75deaf25848a16799695bab62068c579d
mhmddorgham <mohammed.dorgham25@gmail.com>
2022-12-24 00:08:45 +0400

    Set Dynamic Port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const https = require('https');
 const bodyParser = require('body-parser');
-
+const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -53,6 +53,6 @@ app.post("/", (req, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 })
